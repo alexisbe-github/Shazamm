c88524d3c8afdc90615461df8bd3a827c13c530b
mi-1000 <119438442+mi-1000@users.noreply.github.com>
2023-02-14 12:23:21 +0100

    Ajout de la connexion à la BDD

---
+++ src/main/java/model/dao/Connexion.java
@@ -0,0 +1,61 @@
+package main.java.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import main.resources.utils.EnvironmentVariablesUtils;
+
+/**
+ * Connexion à une base de données
+ * 
+ * @author Emile
+ *
+ */
+public class Connexion {
+
+	private static Connexion instance = null;
+	private Connection con;
+
+	/**
+	 * Se connecte à la base de données spécifiée dans le fichier <code>ENVIRONMENT.properties</code>.
+	 * 
+	 * @see main.resources.utils.EnvironmentVariablesUtils
+	 */
+	private Connexion() {
+
+		try {
+
+			this.con = DriverManager.getConnection(EnvironmentVariablesUtils.getBDDURL(),
+					EnvironmentVariablesUtils.getBDDUSER(), EnvironmentVariablesUtils.getBDDMDP());
+
+		} catch (SQLException e) {
+
+		}
+
+	}
+
+	/**
+	 * Singleton renvoyant la connexion.
+	 * 
+	 * @return La connexion
+	 */
+	public static Connexion getInstance() {
+		if (instance == null) {
+			instance = new Connexion();
+		}
+		return instance;
+	}
+
+	/**
+	 * Ferme la connexion.
+	 */
+	public void fermerConnexion() {
+		try {
+			this.con.close();
+		} catch (SQLException e) {
+
+		}
+	}
+
+}
