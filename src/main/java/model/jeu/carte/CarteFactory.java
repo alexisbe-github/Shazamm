5b508ab18fd124c760846469163ed4de4cab8532
Baptiste <billardbaptiste@hotmail.fr>
2023-02-16 19:32:10 +0100

    Create Class CarteFactory (pending)

---
+++ src/main/java/model/jeu/carte/CarteFactory.java
@@ -0,0 +1,39 @@
+package main.java.model.jeu.carte;
+
+public class CarteFactory {
+	
+	public Carte createCarte(String nomCarte) {
+		if(nomCarte.equals("Mutisme")) {
+			return null;
+		}else if(nomCarte.equals("Clone")) {
+			return null;
+		}else if(nomCarte.equals("Larcin")) {
+			return null;
+		}else if(nomCarte.equals("Fin de Manche")) {
+			return null;
+		}else if(nomCarte.equals("Milieu")) {
+			return null;
+		}else if(nomCarte.equals("Recyclage")) {
+			return null;
+		}else if(nomCarte.equals("Boost Attaque")) {
+			return null;
+		}else if(nomCarte.equals("Double dose")) {
+			return null;
+		}else if(nomCarte.equals("Qui Perd Gagne")) {
+			return null;
+		}else if(nomCarte.equals("Braisier")) {
+			return null;
+		}else if(nomCarte.equals("Résistance")) {
+			return null;
+		}else if(nomCarte.equals("Harpagon")) {
+			return null;
+		}else if(nomCarte.equals("Boost réserve")) {
+			return null;
+		}else if(nomCarte.equals("Aspiration")) {
+			return null;
+		}else {
+			return null;
+		}
+	}
+	
+}
