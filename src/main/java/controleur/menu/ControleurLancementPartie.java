8c7ac5aeeaf5cb5318beeb99f24a5d90ad2e2049
Baptiste <billardbaptiste@hotmail.fr>
2023-05-12 17:13:09 +0200

    controleur lancement partie

---
+++ src/main/java/controleur/menu/ControleurLancementPartie.java
@@ -0,0 +1,74 @@
+package main.java.controleur.menu;
+
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+import main.java.model.bdd.Profil;
+import main.java.model.jeu.Chrono;
+import main.java.model.jeu.ECouleurJoueur;
+import main.java.model.jeu.Joueur;
+import main.java.model.jeu.partie.Partie;
+import main.java.vue.jeu.VueJeu;
+import main.java.vue.profil.VueLancementPartie;
+
+public class ControleurLancementPartie implements ActionListener{
+
+	VueLancementPartie vlp;
+	JTextField saisieTemps;
+	Chrono timer;
+	ECouleurJoueur cj1;
+	ECouleurJoueur cj2;
+	
+	public ControleurLancementPartie(VueLancementPartie vlp, JTextField saisie, Chrono timer, ECouleurJoueur cj1, ECouleurJoueur cj2) {
+		this.vlp=vlp;
+		this.timer=timer;
+		this.saisieTemps = saisie;
+		this.cj1 = cj1;
+		this.cj2 = cj2;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Profil pj1 = vlp.getVueProfilJ1().getPanelSelection().getProfilSelectionne();
+		Profil pj2 = vlp.getVueProfilJ2().getPanelSelection().getProfilSelectionne();
+		int time = 0;
+		try {
+			time = Integer.parseInt(saisieTemps.getText());
+			timer.setDuree(time);
+		}catch(NumberFormatException ex) {
+			
+		}
+		if(pj1.getId()!=pj2.getId() && pj1!=null && pj2!=null && time>=10 && time<600) {
+			
+			lancerPartie(pj1,pj2);
+		}
+	}
+	
+	public void lancerPartie(Profil pj1, Profil pj2) {
+		Joueur joueur1 = new Joueur(cj1, pj1);
+		Joueur joueur2 = new Joueur(cj2, pj2);
+		
+		Partie p = new Partie(joueur1, joueur2);
+		
+		VueJeu fenetreJ1 = new VueJeu(joueur1, p, timer);
+		VueJeu fenetreJ2 = new VueJeu(joueur2, p, timer);
+		
+		int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+		fenetreJ1.setLocation(new Point(0, 0));
+		fenetreJ2.setLocation(new Point(width / 2, 0));
+		
+		if (joueur1.getCouleur().equals(ECouleurJoueur.VERT)) {
+			p.setStrategy(fenetreJ1, fenetreJ2);
+		} else {
+			p.setStrategy(fenetreJ2, fenetreJ1);
+		}
+		
+		p.addObserver(fenetreJ1);
+		p.addObserver(fenetreJ2);
+	}
+	
+}
